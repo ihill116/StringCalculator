5360a2f3b6c4dabf8c1d3567309eef1edf2eef37
Isaak Hill <Isaak@Dragon.local>
2018-01-30 11:36:54 -0500

    + Added tests for single numbers, making sure that numbers with multiple digits work

--- test/com/ihill/StringCalculatorTest.java
+++ test/com/ihill/StringCalculatorTest.java
@@ -11,6 +11,13 @@ class StringCalculatorTest {
 
     @org.junit.jupiter.api.Test
     void addTest() {
+        //Test for empty string
         assertEquals(0, sc.add(""));
+
+        //Tests for 1 number in string
+        assertEquals(0,sc.add("0"));
+        assertEquals(5,sc.add("5"));
+        assertEquals(50, sc.add("50"));
+        assertEquals(500, sc.add("500"));
     }
 }
